1e7aa00508cfa94a329aab996d19c0ee7f94f0fa
ianzhonee <nasipadang240817@gmail.com>
2025-12-19 09:00:33 +0700

    Update limiter.py

--- .github/workflows/utils/limiter.py
+++ .github/workflows/utils/limiter.py
@@ -2,17 +2,12 @@ import time
 
 class PeakRPMLimiter:
     def __init__(self, max_rpm):
-        # Menghitung jeda minimum antar permintaan agar tidak melebihi RPM
-        self.min_interval = 60.0 / max_rpm 
+        self.interval = 60.0 / max_rpm
         self.last_call = 0
 
     def secure_call(self):
-        """Panggil fungsi ini setiap kali sebelum melakukan request API"""
         now = time.time()
         elapsed = now - self.last_call
-        
-        if elapsed < self.min_interval:
-            wait_time = self.min_interval - elapsed
-            time.sleep(wait_time)
-        
+        if elapsed < self.interval:
+            time.sleep(self.interval - elapsed)
         self.last_call = time.time()
